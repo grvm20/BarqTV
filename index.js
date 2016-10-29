c102f908b58526cb8f0ecefa838c56b7d92d1e74
sarangkarpate <karpatesj@gmail.com>
2016-10-29 17:56:01 -0400

    Demo change for resolving merge conflict

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ var mapping = require('./error-mapping');
 
 // Functions.
 
-function injectDependencies() {
+const injectDependencies = (customCustomerDao, customAddressDao, customAddressNormalizeSao) => {
   console.log('Injecting dependencies.');
   customerDao = new Dao(CUSTOMERS_TABLE_NAME, DYNAMO_DOC_ClIENT);
   addressDao = new Dao(ADDRESSES_TABLE_NAME, DYNAMO_DOC_ClIENT);
