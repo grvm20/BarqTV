c4c5ca9ae1bf3d7d89793ff41e92e3bc368eb489
PlabanM1 <Plaban.Mohanty92@gmail.com>
2016-11-16 18:23:31 -0500

    Added CORS

--- ui/js/controllers/testcontrol2.js
+++ ui/js/controllers/testcontrol2.js
@@ -4,6 +4,7 @@ var liveaddress = $.LiveAddress({
   target: "US",
   addresses: [{
     address1: '#street-address',
+    address1: '#street-address2',
     locality: '#city',
     administrative_area: '#state',
     }]
@@ -14,30 +15,30 @@ function PostTest() {
     url: 'https://j3z4xwt1ch.execute-api.us-west-2.amazonaws.com/prod/customers',
     type: 'POST',
     data: JSON.stringify({
-      last_name : $('#lastname').val(),
+     /* last_name : $('#lastname').val(),
       first_name : $('#firstname').val(),
       email : $('#email').val(),
       phone_number : $('#phonenumber').val(),
       address : {
         city : $('#city').val(),
-        state : $('#state').val(),
+        state : $('#state').val(), 
         apt : $('#apt').val(),
         number : $('#number').val(),
         street : $('#streetname').val(),        
         zip_code : $('#zip').val()
+      }*/
+      last_name : "uitest",
+      first_name : "test",
+      email : "nope@gmail.com",
+      phone_number : "1234567890",
+      address : {k
+        city : "New York",
+        state : "NY",
+        apt : "14",
+        street : "Claremont Ave",
+        number : "188",
+        zip_code : "10027"
       }
-      // last_name : "uitest",
-      // first_name : "test",
-      // email : "uitest@gmail.com",
-      // phone_number : "1234567890",
-      // address : {
-      //   city : "New York",
-      //   state : "NY",
-      //   apt : "64",
-      //   street : "1",
-      //   number : "1",
-      //   zip_code : "10025"
-      // }
     }),
     dataType: 'json',
     contentType: 'application/json',
@@ -47,14 +48,16 @@ function PostTest() {
       console.log(textStatus);
       
       $('#response pre').html("success");
-      alert("Success");
+      //alert("Success");
     },
     error: function (xhr, ajaxOptions, thrownError) {
-              //$('#response pre').html("error");
+              $('#response pre').html("error");
               console.log(xhr);
               console.log(ajaxOptions);
-              alert(thrownError);
-            }
+              console.log(thrownError);
+              //alert(thrownError);
+            },
+    crossDomain: true
           });
 
   document.getElementById('firstForm').reset(); 
